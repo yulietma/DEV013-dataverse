8537462bc539fb0782504c7ad2bb3f8aa5744e68
YenCastro <castroyenny74@gmail.com>
2024-01-25 13:35:19 -0500

    Agregamos URL a la data

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -4,7 +4,7 @@ export default [
     "name": "Pinocho",
     "shortDescription": "Muñeco de madera que cobra vida",
     "description": "Pinocho es un muñeco de madera tallado por Geppetto que cobra vida gracias a la magia de una hada. A lo largo de su historia, Pinocho enfrenta diversas aventuras y aprende valiosas lecciones sobre la honestidad y la responsabilidad.",
-    "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+    "imageUrl": "https://th.bing.com/th/id/OIG.Hx9k0_XgQwnbWEsPAQnf?pid=ImgGn",
     "facts": {
         "creator": "Geppetto",
         "magicalBeing": "Hada Azul",
@@ -22,7 +22,7 @@ export default [
   "name": "El Elefante Bernardo",
   "shortDescription": "Gigante y amigable elefante",
   "description": "El Elefante Bernardo es un gigante elefante que vive en la selva. A pesar de su imponente tamaño, es conocido por su naturaleza amigable y su amor por la naturaleza.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG3.o7I461DRPog9_U20saVV?pid=ImgGn",
   "facts": {
       "habitat": "Selva",
       "personality": "Amigable y juguetón",
@@ -36,7 +36,7 @@ export default [
   "name": "Uga la Tortuga",
   "shortDescription": "Anciana y sabia tortuga",
   "description": "Uga es una tortuga anciana conocida por su sabiduría y paciencia. Vive en un estanque tranquilo donde a menudo aconseja a otros animales con sus experiencias de vida.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.4cMd5Q7B07Hb3fD7_amy?pid=ImgGn",
   "facts": {
       "habitat": "Estanque",
       "age": "Más de 100 años",
@@ -50,7 +50,7 @@ export default [
   "name": "La Liebre y la Tortuga",
   "shortDescription": "Personajes de la famosa fábula",
   "description": "La Liebre y la Tortuga son personajes protagonistas de la famosa fábula. La historia ilustra la importancia de la perseverancia y la constancia.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.22ARtDnxN8DdvuwaTf3B?pid=ImgGn",
   "facts": {
       "moral": "La constancia vence a la rapidez",
       "lesson": "No subestimes a los demás",
@@ -63,7 +63,7 @@ export default [
   "name": "El Huevo que Pía",
   "shortDescription": "Un huevo especial con sorpresa",
   "description": "Este huevo es especial porque, en lugar de romperse para revelar a un pollito, ¡pía! Es una rareza entre los huevos y ha cautivado la curiosidad de todos en la granja.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.cJTRrmCVSPuCqzwD5a9V?pid=ImgGnhttps://th.bing.com/th/id/OIG.cJTRrmCVSPuCqzwD5a9V?pid=ImgGn",
   "facts": {
       "uniqueTrait": "Pía en lugar de romperse",
       "mystery": "Origen desconocido",
@@ -76,7 +76,7 @@ export default [
   "name": "El Patito Feo",
   "shortDescription": "Pequeño patito con un destino sorprendente",
   "description": "El Patito Feo es un pequeño patito que, a pesar de ser diferente al principio, descubre su verdadera identidad como un hermoso cisne. La historia destaca la importancia de la autoaceptación.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.trUAtSaEEfBWO5uOTztQ?pid=ImgGn",
   "facts": {
       "origin": "Nació en un nido de patos",
       "transformation": "Descubre ser un cisne",
@@ -90,7 +90,7 @@ export default [
   "name": "El Osito Curioso",
   "shortDescription": "Aventuras de un osito que ama aprender",
   "description": "Acompaña al Osito Curioso en sus emocionantes aventuras mientras explora el bosque y descubre el fascinante mundo que lo rodea.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.ooZJ3fAbRb4qUtqYHx65?pid=ImgGn",
   "facts": {
       "mainCharacter": "Osito Curioso",
       "adventures": "Explorando el bosque",
@@ -104,7 +104,7 @@ export default [
   "name": "La Aventura del Pececito Azul",
   "shortDescription": "Un pececito curioso explora el océano",
   "description": "Acompaña al Pececito Azul en su emocionante aventura por el océano, donde descubre la importancia de la amistad y la superación.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.vvwIdPTH10GErdhizrqo?pid=ImgGn",
   "facts": {
       "mainCharacter": "Pececito Azul",
       "setting": "Océano",
@@ -118,7 +118,7 @@ export default [
   "name": "Las Travesuras de la Ratona Rosita",
   "shortDescription": "Ratona traviesa en busca de diversión",
   "description": "Acompaña a la Ratona Rosita mientras se embarca en travesuras encantadoras y aprende valiosas lecciones sobre la amistad y la responsabilidad.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.ut23lB.pqFl2rWuTU4g1?pid=ImgGn",
   "facts": {
       "mainCharacter": "Ratona Rosita",
       "adventures": "Travesuras encantadoras",
@@ -132,7 +132,7 @@ export default [
   "name": "Cenicienta y sus Amigos Animales",
   "shortDescription": "Una versión encantadora del clásico cuento",
   "description": "Revive la historia de Cenicienta con un toque especial, destacando la amistad con sus adorables amigos animales que la ayudan a cumplir sus sueños.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.HrtZwjx795WPJHIxWauj?pid=ImgGn",
   "facts": {
       "mainCharacter": "Cenicienta",
       "friends": "Animales encantadores",
@@ -146,7 +146,7 @@ export default [
   "name": "El Gato con Botas y la Aventura en el Reino",
   "shortDescription": "El astuto gato que conquista el corazón del reino",
   "description": "Acompaña al Gato con Botas en una emocionante aventura para ganarse el corazón del reino y demostrar que la astucia puede vencer a cualquier adversidad.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG2.j4NowxnjROOoSoPbvsxZ?pid=ImgGn",
   "facts": {
       "mainCharacter": "Gato con Botas",
       "adventure": "Conquistando el reino",
@@ -160,7 +160,7 @@ export default [
   "name": "Blancanieves y los Siete Enanitos",
   "shortDescription": "La historia mágica de amistad y superación",
   "description": "Descubre la mágica historia de Blancanieves, su amistad con los siete enanitos y cómo supera las adversidades con valentía y bondad.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG3..kOh.Mpjmqdq7JgPjcWH?pid=ImgGn",
   "facts": {
       "mainCharacter": "Blancanieves",
       "friends": "Siete enanitos",
@@ -174,7 +174,7 @@ export default [
   "name": "El Lobo y los Tres Cerditos",
   "shortDescription": "Aventuras de los cerditos y el astuto lobo",
   "description": "Sumérgete en las divertidas aventuras de los tres cerditos y el astuto lobo mientras construyen sus casas y enfrentan desafíos inesperados.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG2.JH4vKuk8UrOIFOwKx.Q3?pid=ImgGn",
   "facts": {
       "mainCharacters": ["Cerdito1", "Cerdito2", "Cerdito3", "Lobo"],
       "adventures": "Construyendo casas y superando desafíos",
@@ -188,7 +188,7 @@ export default [
   "name": "La Sirenita y sus Amigos Marinos",
   "shortDescription": "La historia mágica de la sirenita y sus amigos bajo el mar",
   "description": "Explora el mágico mundo submarino con la Sirenita y sus amigos marinos mientras descubren tesoros y enfrentan desafíos emocionantes.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG1.WgDkarCNSDkZfW7nUJRl?pid=ImgGn",
   "facts": {
       "mainCharacter": "La Sirenita",
       "friends": ["Pez Amigo1", "Pez Amigo2", "Cangrejo Amigo"],
@@ -203,7 +203,7 @@ export default [
   "name": "Hansel y Gretel en el Bosque Encantado",
   "shortDescription": "Aventuras de los hermanos en un bosque lleno de magia",
   "description": "Acompaña a Hansel y Gretel mientras se aventuran en un bosque encantado lleno de magia, dulces y desafíos inesperados.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.5SCkT9zOjtYGs1yRwulG?w=1024&h=1024&rs=1&pid=ImgDetMaingit",
   "facts": {
       "mainCharacters": ["Hansel", "Gretel", "Bruja Malvada"],
       "magicalForest": true,
@@ -217,7 +217,7 @@ export default [
   "name": "El León y el Ratón Amigo",
   "shortDescription": "Una historia de amistad improbable entre un león y un ratón",
   "description": "Descubre cómo la pequeña amistad entre un león majestuoso y un ratón valiente demuestra que la ayuda puede venir de los lugares menos esperados.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG2.Ogz8cNU1BkECJvzqIXho?w=1024&h=1024&rs=1&pid=ImgDetMain",
   "facts": {
       "mainCharacters": ["León Majestuoso", "Ratón Valiente"],
       "lesson": "La amistad no tiene tamaño",
@@ -230,7 +230,7 @@ export default [
   "name": "La Bella Durmiente y el Reino de los Sueños",
   "shortDescription": "Una versión encantada del clásico cuento de hadas",
   "description": "Acompaña a la Bella Durmiente en su viaje mágico a través del reino de los sueños, donde descubre el poder de la amistad y el amor verdadero.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG3.eLzUSyzgDIEiMkmnTil5?w=1024&h=1024&rs=1&pid=ImgDetMain",
   "facts": {
       "mainCharacter": "Bella Durmiente",
       "enchantedRealm": true,
@@ -244,7 +244,7 @@ export default [
   "name": "Pulgarcito y los Secretos del Bosque Encantado",
   "shortDescription": "Aventuras del pequeño Pulgarcito en un bosque lleno de sorpresas",
   "description": "Acompaña a Pulgarcito mientras descubre los secretos fascinantes del bosque encantado y demuestra que incluso los más pequeños pueden tener grandes aventuras.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG2.sM5h9xN07xF43Wr6Cfdw?w=1024&h=1024&rs=1&pid=ImgDetMain",
   "facts": {
       "mainCharacter": "Pulgarcito",
       "enchantingForest": true,
@@ -258,7 +258,7 @@ export default [
   "name": "Peter Pan y la Tierra de Nunca Jamás",
   "shortDescription": "Las emocionantes aventuras de Peter Pan y sus amigos en la tierra de la fantasía",
   "description": "Vuela con Peter Pan y sus amigos a la Tierra de Nunca Jamás, donde la magia y la amistad duran para siempre, y donde enfrentan al astuto Capitán Garfio.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG2.Kqb50ddaIBE8fvSXIERm?pid=ImgGn",
   "facts": {
       "mainCharacter": "Peter Pan",
       "enchantedLand": "Tierra de Nunca Jamás",
@@ -268,25 +268,26 @@ export default [
   }
 },
 {
-  "id": "PatitoFeoBuscaIdentidad",
-  "name": "El Patito Feo en Busca de su Identidad",
-  "shortDescription": "La conmovedora historia del patito que descubre su verdadera belleza",
-  "description": "Acompaña al Patito Feo en su viaje de autodescubrimiento mientras supera los desafíos y abraza su verdadera identidad llena de belleza interior.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
-  "facts": {
-      "mainCharacter": "Patito Feo",
-      "selfDiscovery": true,
-      "lesson": "La verdadera belleza viene de adentro",
-      "curiousFact1": "Incomprendido por los demás animales",
-      "curiousFact2": "Transformación en un hermoso cisne"
-  }
+  "id": "AventuraZapatosMagicos",
+    "name": "La Aventura de los Zapatos Mágicos en el País de los Calcetines Coloridos",
+    "shortDescription": "Un cuento encantado sobre zapatos que cobran vida en un mundo lleno de calcetines de colores",
+    "description": "Acompaña a unos zapatos mágicos mientras emprenden una aventura inesperada en el País de los Calcetines Coloridos, donde descubren la importancia de la diversidad y la cooperación.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.kUS4XL9v0zMPq3m4qEMP?w=1024&h=1024&rs=1&pid=ImgDetMain",
+    "facts": {
+        "mainCharacters": ["Zapato1", "Zapato2", "Zapato3"],
+        "enchantedWorld": "País de los Calcetines Coloridos",
+        "adventure": "Explorando y haciendo nuevos amigos",
+        "lesson": "La diversidad hace que el mundo sea más vibrante",
+        "curiousFact1": "Cada zapato tiene un cordón mágico",
+        "curiousFact2": "Los calcetines tienen personalidades únicas"
+    }
 },
 {
   "id": "FlautistaHamelinAventuraMusical",
   "name": "El Flautista de Hamelín y la Aventura Musical",
   "shortDescription": "La clásica historia del flautista que encanta a toda la ciudad",
   "description": "Descubre cómo el talentoso Flautista de Hamelín utiliza su música para resolver problemas y llevar alegría a la ciudad en esta aventura musical encantadora.",
-  "imageUrl": "URL_DE_LA_IMAGEN_GENERADA",
+  "imageUrl": "https://th.bing.com/th/id/OIG.ZLFi5eNV5PHT45G8H.6C?pid=ImgGn",
   "facts": {
       "mainCharacter": "Flautista de Hamelín",
       "musicalAdventure": true,
@@ -294,5 +295,48 @@ export default [
       "curiousFact1": "Encanta a ratones y humanos con su flauta",
       "curiousFact2": "Resuelve conflictos a través de la música"
   }
-}
+},
+{
+  "id": "GirasolTraviesoJardinEncantado",
+  "name": "El Girasol Travieso en el Jardín Encantado",
+  "shortDescription": "Aventuras de un girasol que cobra vida en un jardin magico",
+  "description": "Acompaña al Girasol Travieso mientras explora un jardín encantado, descubre amigos sorprendentes y aprende sobre la importancia de la alegría y la amistad.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.6E_sPKJI5tu.Y.n0_g2Y?pid=ImgGn",
+  "facts": {
+      "mainCharacter": "Girasol Travieso",
+      "enchantedGarden": true,
+      "adventures": "Explorando y haciendo travesuras en el jardín",
+      "curiousFact1": "Sus pétalos brillan cuando está feliz",
+      "curiousFact2": "Tiene una risa contagiosa"
+  }
+  },
+  
+  {
+    "id": "LibroColoresOlvidados",
+    "name": "El Libro de los Colores Olvidados",
+    "shortDescription": "Una historia mágica sobre colores que cobran vida",
+    "description": "Descubre la encantadora historia de un libro especial que revive colores olvidados y cómo estos colores transforman el mundo de manera mágica.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.QR2ywz.2RjTBEI2teDzt?pid=ImgGn",
+    "facts": {
+        "mainCharacter": "Libro Mágico",
+        "magicalColors": true,
+        "lesson": "La diversidad y la creatividad hacen el mundo más hermoso",
+        "curiousFact1": "Cada color tiene su propia personalidad",
+        "curiousFact2": "Los personajes viajan por diferentes paisajes de colores"
+    }
+  },
+{
+    "id": "IslaPeluchesPerdidos",
+    "name": "La Isla de los Peluches Perdidos",
+    "shortDescription": "Aventuras de peluches en una isla mágica",
+    "description": "Acompaña a un grupo de peluches perdidos en una isla mágica mientras buscan un nuevo hogar, enfrentan desafíos y descubren la importancia de la amistad duradera.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.oqXndIJxJtMe7_sXLk2K?pid=ImgGn",
+    "facts": {
+        "mainCharacters": ["Peluche1", "Peluche2", "Peluche3"],
+        "magicalIsland": true,
+        "adventures": "Buscando un nuevo hogar y enfrentando desafíos",
+        "curiousFact1": "Cada peluche tiene una habilidad única",
+        "curiousFact2": "La isla tiene paisajes y criaturas mágicas"
+    }
+  }
 ];
